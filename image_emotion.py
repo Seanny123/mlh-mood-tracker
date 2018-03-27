1eeaaa4bb9b9cf6492560b53f97f955e2ab050fe
Sean Aubin <seanaubin@gmail.com>
2018-03-27 18:30:02 -0400

    got basics working

---
+++ image_emotion.py
@@ -0,0 +1,41 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"/home/saubin/credentials/Learning Night-8b61b06f75e6.json"
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'data/images/happy.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+response = client.face_detection(image=image)
+faces = response.face_annotations
+
+# Names of likelihood from google.cloud.vision.enums
+likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                    'LIKELY', 'VERY_LIKELY')
+print('Faces:')
+
+for face in faces:
+    print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+    print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+    print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+    vertices = (['({},{})'.format(vertex.x, vertex.y)
+                for vertex in face.bounding_poly.vertices])
+
+    print('face bounds: {}'.format(','.join(vertices)))
